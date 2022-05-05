34e440c77464b2bf801e1ab1645543cc36d72827
Saikat Das <saikatdas0790@gmail.com>
2022-05-05 22:24:51 +0530

    Add Timer button with text in the center of the page Fixes #13

---
+++ src/component/molecule/timer/AddNewTimerCounter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import PlusCircle from "$component/atom/icon/heroicons/outline/PlusCircle.svelte";
+</script>
+
+<button
+  on:click
+  class="flex flex-col items-center justify-center gap-4 border-2 border-blue-100 rounded-lg w-80 h-80 cursor-pointer text-blue-400  transition-color transition-shadow hover:bg-slate-700 active:bg-slate-900 shadow-md shadow-blue-200 hover:shadow-xl hover:shadow-blue-200 active:shadow active:shadow-blue-200">
+  <PlusCircle className="h-20 w-20" />
+  <span class="text-3xl font-medium">Add Counter</span>
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-   to read the documentation
-</p>
+<script lang="ts">
+  import AddNewTimerCounter from "$component/molecule/timer/AddNewTimerCounter.svelte";
+  import { addEmptyCounterTimerToList, type Timer } from "./_index";
+
+  let timers: Timer[] = [];
+</script>
+
+<svelte:head>
+  <title>Timer Home</title>
+</svelte:head>
+
+{#if timers.length === 0}
+  <div class="grid place-items-center min-h-screen">
+    <AddNewTimerCounter
+      on:click={() => (timers = addEmptyCounterTimerToList(timers))} />
+  </div>
+{:else}
+  <h1>Hello world</h1>
+{/if}
