ba323a0708d3e1920716086a1fe404f3da480948
Saikat Das <saikatdas0790@gmail.com>
2022-06-02 12:47:10 +0530

    style: adjust counter size to accomodate 3 digits

--- src/component/atom/display/CounterDisplay.svelte
+++ src/component/atom/display/CounterDisplay.svelte
@@ -8,7 +8,7 @@
 
 {#key $timer.context.currentCount}
   <span
-    class="bg-clip-text text-transparent bg-gradient-to-tr from-red-500  to-violet-500 text-9xl font-bold"
+    class="bg-clip-text text-transparent bg-gradient-to-tr from-red-500 to-violet-500 text-8xl font-bold transform -translate-y-2"
     in:fly={{ y: 20, duration: 100 }}>
     {$timer.context.currentCount}
   </span>
--- src/component/molecule/counter/Counter.svelte
+++ src/component/molecule/counter/Counter.svelte
@@ -14,7 +14,7 @@
     className="inline-flex items-center justify-center p-4 rounded-full hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600"
     on:click={() => timer.send("TIMER_COUNTER_DECREMENTED")}>
     <span class="sr-only">Decrement counter by 1</span>
-    <Minus className="h-12 w-12 text-red-500" />
+    <Minus className="h-10 w-10 text-red-500" />
   </CounterModify>
 
   <CounterDisplay {timer} />
@@ -23,6 +23,6 @@
     className="inline-flex items-center justify-center p-4 rounded-full hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-violet-600"
     on:click={() => timer.send("TIMER_COUNTER_INCREMENTED")}>
     <span class="sr-only">Increment counter by 1</span>
-    <Plus className="h-12 w-12 text-purple-500" />
+    <Plus className="h-10 w-10 text-purple-500" />
   </CounterModify>
 </div>
