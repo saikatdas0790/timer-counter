bc245bc1a66c11ac0cfdd5d2b6a2f79cd6896723
Saikat Das <saikatdas0790@gmail.com>
2022-06-20 18:36:19 +0530

    chore: remove offending icon from manifest

--- pwa-configuration.js
+++ pwa-configuration.js
@@ -24,12 +24,6 @@ const pwaConfiguration = {
         sizes: "512x512",
         type: "image/png",
       },
-      {
-        src: "/pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
     ],
   },
   workbox: {
